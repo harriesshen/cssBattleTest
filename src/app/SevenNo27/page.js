d533d21292243847be53a99696331c48a139a0c5
harries4206 <harries4206@gmail.com>
2023-07-27 18:20:35 +0800

    設定首頁css,加入7/27的cssbattle

---
+++ src/app/SevenNo27/page.js
@@ -0,0 +1,14 @@
+import "./page.scss";
+
+const Seven_twentySeven = () => {
+  return (
+    <div id="container-0727">
+      <div className="container">
+        <div className="darkBlueTriangle"></div>
+        <div className="whiteTriangle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Seven_twentySeven;
